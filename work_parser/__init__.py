51e7b91ff879b89b6b05eb96af658cb71fb36eea
Eugene <e.babay@flyaps.com>
2024-09-23 21:53:11 +0300

    Commit

---
+++ work_parser/__init__.py
@@ -0,0 +1,11 @@
+from .dto import Vacancy
+from .export import ExportEngine
+from .parser import ParserEngine
+from .request import RequestEngine
+
+__all__ = [
+    "RequestEngine",
+    "Vacancy",
+    "ExportEngine",
+    "ParserEngine",
+]
